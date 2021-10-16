0cf865170eea137ea003b4a7888acae46fa5a861
matanrotgolz <matanrotgoltz@gmail.com>
2021-10-16 20:44:12 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let pythonbar = document.getElementById('python');
+pythonbar.style.width = "100%";
+
+let CPlusBar = document.getElementById('C++');
+CPlusBar.style.width = "70%";
+
+let cBar = document.getElementById('C');
+cBar.style.width = "50%";
+
+let htmlBar = document.getElementById('HTML');
+htmlBar.style.width = '100%';
+
+let vhdlBar = document.getElementById('vhdl');
+vhdlBar.style.width = '60%';
+
+ let cssBar = document.getElementById('css');
+ cssBar.style.width = '100%';
